a6e0e22c90721709550015c5a5f0ea32383954f2
graysoncharat <graysoncharat@localhost>
2023-03-13 19:59:04 -0700

    DrumKit, MyFirstSwingGUI, PhotoQuiz

--- src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
+++ src/_03_gui_with_help/_2_photo_quiz/PhotoQuiz.java
@@ -26,35 +26,48 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address” )
-
+		String url = "https://www.amnh.org/var/ezflow_site/storage/images/media/amnh/images/explore/ology-images/brain/optical-illusions/op-illusions-listing-hero/3877180-1-eng-US/op-illusions-listing-hero_twittershare_1024.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+		Component component = createImage(url);
 		// 3. use the "createImage()" method below to initialize your Component
 
 		// 4. add the image to the quiz window
-
+		quizWindow.add(component);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String question = JOptionPane.showInputDialog(null, "Is this image moving or staying still?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if(question.equalsIgnoreCase("staying still")) {
+			System.out.println("CORRECT");
+		}else {
+			System.out.println("INCORRECT");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+		quizWindow.remove(component);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+		String urlTwo = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Checker_shadow_illusion.svg/200px-Checker_shadow_illusion.svg.png";
+		Component componentTwo = createImage(urlTwo);
 		// 11. add the second image to the quiz window
-
+			quizWindow.add(componentTwo);
 		// 12. pack the quiz window
-
+			quizWindow.pack();
 		// 13. ask another question
-
+			String questionTwo = JOptionPane.showInputDialog(null, "Is square A and square B the same or different color?");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+			if(questionTwo.equalsIgnoreCase("same")) {
+				System.out.println("CORRECT");
+			}else if(questionTwo.equalsIgnoreCase("same color")) {
+				System.out.println("CORRECT");
+			}else if(questionTwo.equalsIgnoreCase("the same color")) {
+				System.out.println("CORRECT");
+			}else {			
+				System.out.println("INCORRECT");
+			}
 	}
 
 	private Component createImage(String imageUrl) {
--- src/_03_gui_with_help/_3_drum_kit/DrumKit.java
+++ src/_03_gui_with_help/_3_drum_kit/DrumKit.java
@@ -6,8 +6,10 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 import game_tools.Sound;
 
@@ -16,33 +18,35 @@ public class DrumKit implements MouseListener {
     JLabel drumLabel;
 
     public void run() {
+    	String title = "DrumKit";
         //  Make a JFrame variable and initialize it using "new JFrame()"
-
+    	JFrame frame = new JFrame();
         //  Make the frame visible and
         // set its default close operation to JFrame.EXIT_ON_CLOSE
-
+    	frame.setVisible(true);
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //  Set the title of the frame
-
+    	frame.setTitle(title);
         //  Make a JPanel variable and initialize it using "new JPanel().
-
+    	JPanel panel = new JPanel(); 
         //  Add the panel to the frame. (The panel is invisible.)
-
+    	frame.add(panel);
         //  Use the image of a drum provided ("snare.jpg") or
         //  download an image of a drum from the Internet and drop it into the
         //  Drum Kit recipe package.
-
+    	
         //  Put the name of the drum image file in a String variable.
-
+    	String drumName = "snare.jpg";
         //  Edit the next line to use your String variable
         // drumLabel = createLabelImage(drumImageString);
-
+    	drumLabel = createLabelImage(drumName);
         //  Add the label to the panel
-
+    	panel.add(drumLabel);
         //  Call the pack() method on the frame.
         // Run your program. Do you see your drum image?
-
+    	frame.pack();
         // Add this MouseListener to drumLabel
-
+    	drumLabel.add(MouseListener);
         // *** Write the code in the mouseClicked() method below
 
         //  Set the layout of the panel to "new GridLayout()"
