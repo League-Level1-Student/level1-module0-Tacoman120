c39fcc78c6cbce9229c4de4ab8fe4eaeac0579cc
jeremycui2008 <jeremycuid@gmail.com>
2023-04-07 16:40:36 -0700

    Cuteness TV, Sound Effects Machine

--- src/_04_gui_from_scratch/_3_sound_effects_machine/Sound_Effects_Machine.java
+++ src/_04_gui_from_scratch/_3_sound_effects_machine/Sound_Effects_Machine.java
@@ -2,10 +2,14 @@ package _04_gui_from_scratch._3_sound_effects_machine;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
 
 public class Sound_Effects_Machine implements ActionListener {
 	JFrame frame = new JFrame();
@@ -28,6 +32,10 @@ public class Sound_Effects_Machine implements ActionListener {
 		frame.add(panel);
 		frame.pack();
 		buttonOne.addActionListener(this);
+		buttonTwo.addActionListener(this);
+		buttonThree.addActionListener(this);
+		buttonFour.addActionListener(this);
+		buttonFive.addActionListener(this);
 		
 		
 	}
@@ -35,16 +43,38 @@ public class Sound_Effects_Machine implements ActionListener {
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		if(arg0.getSource() == buttonOne) {
-			
+			playSound("sawing-wood.wav");
 		}else if(arg0.getSource() == buttonTwo) {
-			
+			playSound("tasmanian-devil.wav");
 		}else if(arg0.getSource() == buttonThree) {
-			
+			playSound("hyena-laugh.wav");
 		}else if(arg0.getSource() == buttonFour) {
-			
+			playSound("analog-watch-alarm.wav");
 		}else if(arg0.getSource() == buttonFive) {
-			
+			playSound("alien-spaceship.wav");
 		}
 	}
+	
+	private void playSound(String soundFile) {
+		String path = "src/_04_gui_from_scratch/_3_sound_effects_machine/";
+			File sound = new File(path+soundFile);
+			if (sound.exists()) {
+				new Thread(() -> {
+				try {
+					Clip clip = AudioSystem.getClip();
+					clip.open(AudioSystem.getAudioInputStream(sound));
+					clip.start();
+					Thread.sleep(clip.getMicrosecondLength()/1000);
+				}
+				catch (Exception e) {
+					System.out.println("Could not play this sound");
+				}}).start();
+	 		}
+			else {
+				System.out.println("File does not exist");
+			}
+		
+	}
+    
 	
 }
---
+++ src/_04_gui_from_scratch/_4_cuteness_tv/Cuteness_Tv_Runner.java
@@ -0,0 +1,11 @@
+package _04_gui_from_scratch._4_cuteness_tv;
+
+
+
+public class Cuteness_Tv_Runner {
+	public static void main(String[] args) {
+		
+
+		new CutenessTv().run();
+	}
+}
